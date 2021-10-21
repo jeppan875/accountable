d8c650960acb8279b679664b433406029aac272c
Jesper Hägglund <jeppan875@gmail.com>
2021-10-21 23:13:40 +0200

    navigate to detail

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { View, Pressable, Text, StyleSheet } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigation } from '@react-navigation/core';
 import { selectItem } from '../../store/list/selector';
 import { ApplicationState } from '../../store';
 import { Item } from '../../store/list/types';
 import CardList from '../CardList';
+import { INavigator } from '../../RootNavigator';
 
 const Card = ({
   id,
@@ -15,18 +17,23 @@ const Card = ({
 }) => {
   const [showList, setShowList] = useState(false);
 
+  const navigation: INavigator = useNavigation();
+
   const item: Item = useSelector((state: ApplicationState) =>
     selectItem(state, id),
   );
-  console.log(item, 'itemmmm');
+
   return (
     <View
       style={{
         marginLeft: isChildCard ? 10 : 0,
       }}>
       <Pressable
-        onPress={() => setShowList(!showList)}
-        disabled={!item.hasList}
+        onPress={() =>
+          item.hasList
+            ? setShowList(!showList)
+            : navigation.navigate('Detail', { id })
+        }
         style={({ pressed }) => [styles.root, pressed && { opacity: 0.5 }]}>
         <Text>{item.title}</Text>
         <Text>{item.description}</Text>
